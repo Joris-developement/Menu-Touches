3d42e5825a59542e4bfe6977a45d9cacc9d307e8
Joris <89984107+Joris-developement@users.noreply.github.com>
2022-11-05 18:53:43 +0100

    Update server.lua

--- menu_touches/server.lua
+++ menu_touches/server.lua
@@ -1,9 +1,5 @@
 RegisterCommand('touches', function(source)
     local _src = source
---     InfosPlayer = {
---         id = _src,
---         name = GetPlayerName(_src),
---     }
     TriggerClientEvent('menu_touches:SendNUIMessage', _src, {
         id = _src,
         name = GetPlayerName(_src)
